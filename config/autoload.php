839435469d58a300bc9c63f2400aa3989bd41389
0TshELn1ck <0TshEL_n1ck@i.ua>
2015-11-09 12:00:03 +0200

    added once for required

--- config/autoload.php
+++ config/autoload.php
@@ -1,4 +1,4 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../config/config.php';
+require_once __DIR__ . '/../config/config.php';
 require __DIR__ . '/../App/Database.php';
