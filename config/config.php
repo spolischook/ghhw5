d64c10fb62d8cbca9f5bd177883429c8070a0282
Vladimir Plys <0TshEL_n1ck@i.ua>
2015-11-09 12:57:31 +0200

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,7 +1,1 @@
 <?php
-define('DB_TYPE', 'mysql');
-define('DB_HOST', '94.249.146.189');
-define('DB_NAME', 'users');
-define('DB_USER', 'database user');
-define('DB_PASS', 'database pass');
-define('SIGNATURE', 'TurboSMS signature');
