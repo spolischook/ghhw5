b2f3e714fcee74d340fe9bd122e1366f9ba24bbc
0TshELn1ck <0TshEL_n1ck@i.ua>
2015-11-09 13:06:01 +0200

    refactoring DB query

--- src/TurboSMS.php
+++ src/TurboSMS.php
@@ -17,9 +17,9 @@ class TurboSMS extends Database
     public function sendSMS($recepient, $message)
     {
         $sign = SIGNATURE;
-        $pdo = new Database ("DB_TYPE:host=DB_HOST;dbname=DB_NAME", 'DB_USER', 'DB_PASS');
-        $pdo->query("SET NAMES utf8;");
-        $pdo->query("INSERT INTO 0TshEL_n1ck (`number`,`message`,`sign`)
+        $dbUser = DB_USER;
+        $this->query("SET NAMES utf8;");
+        $this->query("INSERT INTO $dbUser (`number`,`message`,`sign`)
               VALUES ('$recepient','$message', '$sign')");
     }
 
